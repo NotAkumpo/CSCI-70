e6125f1c2af1e4e80b2911229f74506982e917cb
NotAkumpo <abdiel.evangelista@student.ateneo.edu>
2024-09-06 23:08:33 +0800

    Version 2: Added HW 2

---
+++ HW2/ScannerInputPractice.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ScannerInputPractice {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\\Z");
+        String a = scanner.next();
+
+        ScannerPractice myScanner = new ScannerPractice(a);
+
+        try {
+            File output = new File("output.txt");
+            if (output.createNewFile()) {
+                PrintWriter out = new PrintWriter("output.txt");
+
+                //Output here, should still be formatted properly
+                while( !myScanner.printStream().isEmpty() && myScanner.showError() ){
+                    out.println();
+                }
+                out.close();
+
+            }
+            else {
+                System.out.println("An output file already exists. Please delete the current output file.");
+            }
+        } 
+        catch (IOException e){
+            System.out.println("An error occurred.");
+        }
+
+
+
+    }
+
+}
---
+++ HW2/ScannerPractice.java
@@ -0,0 +1,59 @@
+public class ScannerPractice {
+
+    //fields
+    private String inputStream;
+    private char currentChar;
+    private String currentToken;
+    private String currentState;
+    private boolean noError;
+
+    //constructor
+    public ScannerPractice(String inputStream){
+        this.inputStream = inputStream;
+        currentChar = inputStream.charAt(0);
+        currentState = "C";
+        noError = true;
+        currentToken = "";
+    }
+
+    //methods
+    public String printStream(){
+        return inputStream;
+    }
+
+    public boolean showError(){
+        return noError;
+    }
+
+
+    //Prototype only ignore the shit inside
+    public String runDFA(){
+        switch(currentState){
+            case "C":
+                if (Character.isDigit(currentChar)){
+                    updateDFA();
+                    currentState = "N";
+                }
+                switch(currentChar){
+                    case 'X':
+                        updateDFA();
+                        currentState = "A";
+                    case 'Y':
+                        updateDFA();
+                        currentState = "B";
+                }
+        }
+    }
+
+    public String printTokenLexeme(){
+
+    }
+
+    public void updateDFA(){
+        currentToken += currentChar;
+        inputStream = inputStream.substring(1);
+        currentChar = inputStream.charAt(0);
+    }
+
+
+}
